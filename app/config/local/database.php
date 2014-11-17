5faea49ef5a2a1a2c6598b4cb67059ecfbc8c0f8
tom <tbeikman@fas.harvard.edu>
2014-11-17 09:01:55 -0500

    Add DB migration

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,7 +23,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'foobooks',
+			'database'  => 'p4',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',

45525aedb5749ea35260b892659b80b3a2fee35a
tom <tbeikman@fas.harvard.edu>
2014-12-07 00:33:47 -0500

    Minor style update, rewrite the topics view.

--- app/views/topics.blade.php
+++ app/views/topics.blade.php
@@ -5,7 +5,8 @@
 @stop
 
 @section('landingPageIntro')
-Welcome to TBen's Forums, a place for discussion.  See a topic you like? Select the link for that topic and add your .02.  Don't see a topic that catches your interest? Select the Create a Topic link and begin.
+Welcome to TBen's Forums, a place for discussion.  <br>
+See a topic you like? Select the link for that topic and add your .02.  Don't see a topic that catches your interest? Select the Create a Topic link and begin.
 @stop
 
 @section('bodyContent')
@@ -14,23 +15,22 @@ Welcome to TBen's Forums, a place for discussion.  See a topic you like? Select 
     @endif
     <br>
     Here are a list of current topics for discussion...
-<br>
-<br>
-<?php 
-    foreach ($topics as $topic) {
-       //var_dump($topic);  
-       echo "<a href=\"/replies/$topic->id\">Topic: $topic->topic_name</a>";
-       echo "<br>";
-       echo "Description: " . $topic->topic_content . "<br>";
-       $user = DB::table('users')->where('id', $topic->author_id)->first();
-       echo "Author name: " . $user->user_name . "<br>";
-       echo "Created on: " . $topic->created_at . "<br>";
-       echo "<a href=\"/delete/$topic->id\">Delete this topic</a>";
-       echo "<br>";
-       echo "<br>";
-    }
+    <br>
+    <br>
+    @foreach ($topics as $topic) 
+        <div class="list">
+            <a href="/replies/{{$topic->id}}">Topic: {{$topic->topic_name}}</a>
+            <br>
+            Description:  {{$topic->topic_content}} <br>
+            <?php $user = DB::table('users')->where('id', $topic->author_id)->first(); ?>
+            Author name:  {{$user->user_name}} <br>
+            Created on:  {{$topic->created_at }} <br>
+            <a href="/delete/{{$topic->id}}">Delete this topic</a>
+            <br>
+            <br>
+        </div>
+    @endforeach
 
-?>
 @stop
 
 
